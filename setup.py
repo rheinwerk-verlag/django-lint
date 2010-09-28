6178968504fc9eca7f0381dda63c1e13c4e8c0bf
Josh Ourisman <josh@joshourisman.com>
2010-09-28 17:54:06 -0400

    added pylint requirement to setup.py

--- setup.py
+++ setup.py
@@ -23,6 +23,9 @@ setup_args = dict(
         'DjangoLint',
         'DjangoLint.AstCheckers',
     ],
+    required=[
+        'pylint',
+    ]
     author='Chris Lamb',
     author_email='chris@chris-lamb.co.uk',
 )
